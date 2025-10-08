8b8e9393ebb51a0986e140e36ee502972f2607b5
sachin <ssachinshinde7@gmail.com>
2025-10-08 22:35:59 +0530

    latest update pushes

--- LogInpage/src/test/java/CucumberOptions/TestNGRunner.java
+++ LogInpage/src/test/java/CucumberOptions/TestNGRunner.java
@@ -3,14 +3,8 @@ package CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
-
-
-
-@CucumberOptions(features = "src/test/java/FeartureFiles", glue = {
-		"StepDefinations" }, monochrome = true)
+@CucumberOptions(features = "src/test/java/FeartureFiles", glue = { "StepDefinations" }, monochrome = true)
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
-	
 
 }
